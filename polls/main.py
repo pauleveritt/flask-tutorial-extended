b0067deb51f39685df0c6843332e6baf925d52a3
pauleveritt <paul.everitt@jetbrains.com>
2024-05-11 09:56:12 -0400

    Introduce /v1/ into the URL scheme.

--- polls/main.py
+++ polls/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
-api = Api(app)
+api = Api(app, prefix="/v1")
 ns = api.namespace("polls", description="Polls API")
 
 from polls import views  # noqa: E402
